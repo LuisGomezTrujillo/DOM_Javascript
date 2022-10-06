22417a53dedf6c2031c9cecd8f2dd047f39dcd5a
Pocho <luisgomeztrujillo@gmail.com>
2022-10-06 14:30:11 -0500

    Agregado CSS responsive y Menú movil

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-function changeColor(newColor) {
-   // const elem = document.getElementById('para');
+function changeColorTitulo(newColor) {
    const elem1 = document.getElementById('titulo1');
    const elem2 = document.getElementById('titulo2');
+    
     elem1.style.color = newColor;
-    elem2.style.color = newColor;
+    elem2.style.color = newColor; 
+    
   }
+
+function changeColorParrafo(newColor) {
+  const elem = document.getElementById('para');
+  elem.style.color=newColor;
+}
+
+/*
+function changeColorSection(){
+  const 
+}*/
   
+const menuHamIcon=document.querySelector('.menu-ham');
+const mobileMenu=document.querySelector('.mobile-menu');
+
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+mobileMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+  
+  mobileMenu.classList.toggle('inactive');
+}
+
 let firstHeading = document.querySelector('h1');
 
 console.log(firstHeading);
